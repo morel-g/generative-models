40cf7b7cd28fe54eb939e63ffa7fcd00e5cee315
gm <you@example.com>
2023-09-13 19:01:48 +0200

    Update config files

---
+++ score_cifar_config.py
@@ -0,0 +1,23 @@
+from src.case import Case
+from config_files.img.default_img_config import get_default_config
+
+CONFIG = get_default_config()
+CONFIG["data_type"] = Case.cifar10
+CONFIG["model_type"] = Case.score_model
+CONFIG["scheme_params"]["beta_case"] = Case.vanilla
+CONFIG["scheme_params"]["decay_case"] = Case.vanilla_sigma
+CONFIG["training_params"].update(
+    {
+        "epochs": 100,
+        "batch_size": 32,
+        "batch_size_eval": 32,
+    }
+)
+CONFIG["model_params"].update(
+    {
+        "embedding_type": Case.fourier,
+        "n_channels": 64,
+        "ch_mult": (1, 2, 2, 2),
+        "n_resblocks": 2,
+    }
+)
