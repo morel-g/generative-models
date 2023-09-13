56c2db34c9973cb6e2bf1ef3d4de02beb6615d4a
gm <you@example.com>
2023-09-13 11:45:16 +0200

    Update img config file

--- config_files/img/default_img_config.py
+++ config_files/img/default_img_config.py
@@ -2,32 +2,30 @@ from src.case import Case
 import torch
 
 CONFIG = {
-    "data_type": Case.fashion_mnist,  # Case.multimodal_swissroll  #  #  Case.moons  #
+    "data_type": Case.fashion_mnist,
     "model_params": {
         "ch_mult": (1, 2, 4),
-        # "block_types": ("Block2D", "Block2D", "Block2D"),
         "n_channels": 16,
-        "embedding_type": "no_embedding",  #  "linear",  #
-        # "dropout": 0.0,
+        "embedding_type": "no_embedding", 
         "n_resblocks": 2,  #
         # "attn_resolutions": (14,),
     },
     "scheme_params": {
         "T_final": 3.0,
-        "nb_time_steps_train": 200,
-        "nb_time_steps_eval": 200,
+        "nb_time_steps_train": 1000,
+        "nb_time_steps_eval": 1000,
         "adapt_dt": True,
         "beta_case": Case.constant,
         "decay_case": Case.no_decay,
-        "img_model_case": Case.ncsnpp,  # n_net_fashion_mnist,  # ncsnpp,  #
+        "img_model_case": Case.ncsnpp, 
     },
-    "model_type": Case.score_model,  # _critical_damped,
+    "model_type": Case.score_model,
     "training_params": {
         "epochs": 50,
         "batch_size": 32,
         "batch_size_eval": 32,
         "lr": 3e-4,
-        "weight_decay": 0.0,
+        "weight_decay": 1e-3,
         "check_val_every_n_epochs": 10,
         "ema": not True,
         "ema_rate": 0.99,
