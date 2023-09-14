5b14d6f1793d89d10a5ae2a004d6a3dabb158318
gm <you@example.com>
2023-09-14 17:17:44 +0200

    Add some comments to the dataset file

--- src/data_manager/dataset.py
+++ src/data_manager/dataset.py
@@ -1,47 +1,87 @@
 import torch
 from sklearn.model_selection import train_test_split
+from torch.utils.data import DataLoader, Dataset as TorchDataset
+from torchvision import datasets, transforms
+from torchvision.datasets import CIFAR10, FashionMNIST, MNIST
+from ..distribution_toy import inf_train_gen
+from .data_type import toy_data_type
+from ..case import Case
 
-# from datasets import load_dataset, Features, Array2D
+import torch
+from sklearn.model_selection import train_test_split
+
 from torch.utils.data import DataLoader
 from torchvision import datasets, transforms
 from torchvision.transforms import Compose
 from ..distribution_toy import inf_train_gen
 from .data_type import toy_data_type
-from datasets import load_dataset
-from torchvision.datasets import CelebA, CIFAR10, FashionMNIST, MNIST
+from torchvision.datasets import CIFAR10, FashionMNIST, MNIST
 from ..case import Case
 
 
-class Dataset(torch.utils.data.Dataset):
-    "Characterizes a dataset for PyTorch"
+class Dataset(TorchDataset):
+    """
+    Characterizes a dataset for PyTorch.
+    """
 
-    def __init__(self, x, y=None, transform=None):
-        "Initialization"
+    def __init__(
+        self, x, y: torch.Tensor = None, transform: transforms.Compose = None
+    ):
+        """
+        Initializes the Dataset object.
+
+        Parameters:
+        - x (torch.Tensor): Input data.
+        - y (torch.Tensor, optional): Target data.
+        - transform (transforms.Compose, optional): Transformations to be applied on the data.
+        """
         self.x = x
         self.y = y
         self.transform = transform
 
-    def __len__(self):
-        "Denotes the total number of samples"
+    def __len__(self) -> int:
+        """
+        Returns the total number of samples.
+
+        Returns:
+        - int: Total number of samples.
+        """
         len_x = len(self.x) if self.x is not None else 0
         len_y = len(self.y) if self.y is not None else 0
         return max(len_x, len_y)
 
-    def __getitem__(self, index):
-        "Generates one sample of data"
+    def __getitem__(self, index: int):
+        """
+        Generates one sample of data.
+
+        Parameters:
+        - index (int): Index of the data sample.
+
+        Returns:
+        - Tuple[torch.Tensor]: A tuple containing input data and target data if present, otherwise just the input data.
+        """
         x_idx = self.x[index] if self.x is not None else None
 
         if self.y is not None:
-            if self.transform:
-                return self.transform(x_idx), self.transform(self.y[index])
-            return x_idx, self.y[index]
-        else:
-            if self.transform:
-                return self.transform(x_idx)
-            return x_idx
+            return (
+                self.transform(x_idx) if self.transform else x_idx,
+                self.transform(self.y[index])
+                if self.transform
+                else self.y[index],
+            )
+        return self.transform(x_idx) if self.transform else x_idx
 
 
-def compute_mean_and_std(data_type):
+def compute_mean_and_std(data_type: str) -> (torch.Tensor, torch.Tensor):
+    """
+    Computes the mean and standard deviation for a given data type.
+
+    Parameters:
+    - data_type (str): Type of the data.
+
+    Returns:
+    - Tuple[torch.Tensor]: Mean and standard deviation tensors.
+    """
     dataset = prepare_dataset(data_type)[0]
     loader = DataLoader(dataset, batch_size=len(dataset) // 100)
     mean, std = None, None
@@ -57,7 +97,24 @@ def compute_mean_and_std(data_type):
     return mean / nb_samples, std / nb_samples
 
 
-def get_dataset(data_type, log_dir, normalized_img=True, n_samples=None):
+def get_dataset(
+    data_type: str,
+    log_dir: str,
+    normalized_img: bool = True,
+    n_samples: int = None,
+) -> TorchDataset:
+    """
+    Fetches the dataset based on the provided data type and other parameters.
+
+    Parameters:
+    - data_type (str): Type of the data.
+    - log_dir (str): Directory for logs.
+    - normalized_img (bool, optional): Whether to normalize the image. Defaults to True.
+    - n_samples (int, optional): Number of samples, required for 2D datasets.
+
+    Returns:
+    - TorchDataset: The prepared dataset.
+    """
     if data_type in toy_data_type:
         if n_samples in (0, None):
             raise RuntimeError(
@@ -67,57 +124,42 @@ def get_dataset(data_type, log_dir, normalized_img=True, n_samples=None):
     else:
         if not normalized_img:
             return prepare_dataset(data_type)
-        else:
-            mean_std = compute_mean_and_std(data_type)
-            return prepare_dataset(data_type, mean_std=mean_std)
+        mean_std = compute_mean_and_std(data_type)
+        return prepare_dataset(data_type, mean_std=mean_std)
 
 
-# def prepare_dataset(dataset_name="fashion_mnist"):
-#     # load dataset from the hub
-#     dataset = load_dataset(dataset_name)
-#     # define image transformations (e.g. using torchvision)
-#     transform = Compose(
-#         [
-#             transforms.RandomHorizontalFlip(),
-#             transforms.ToTensor(),
-#             transforms.Lambda(lambda t: (t * 2) - 1),
-#         ]
-#     )
+def scale_imgs(t: torch.Tensor) -> torch.Tensor:
+    """
+    Scales the input tensor.
 
-#     # define function
-#     def transforms_func(examples):
-#         examples["pixel_values"] = [
-#             transform(image.convert("L")) for image in examples["image"]
-#         ]
-#         del examples["image"]
-#         return examples
+    Parameters:
+    - t (torch.Tensor): Input tensor.
 
-
-#     # dataset = dataset.map(
-#     #     transforms_func, remove_columns=["image"], batched=True
-#     # ).remove_columns("label")
-#     transformed_dataset = dataset.with_transform(
-#         transforms_func
-#     ).remove_columns("label")
-#     train_data, val_data = (
-#         transformed_dataset["train"],
-#         transformed_dataset["test"],
-#     )
-#     # features = Features({"data": Array2D(shape=(28, 28), dtype="float32")})
-#     return (
-#         train_data,
-#         val_data,  # .set_format(type="torch")
-#     )
-def scale_imgs(t):
+    Returns:
+    - torch.Tensor: Scaled tensor.
+    """
     return (t * 2) - 1
 
 
-def prepare_dataset(name, mean_std=None):
+def prepare_dataset(
+    name: str, mean_std: tuple = None
+) -> (torch.utils.data.Dataset, torch.utils.data.Dataset):
+    """
+    Prepares a dataset based on the given name and normalization parameters.
+
+    Parameters:
+    - name (str): Name of the dataset.
+    - mean_std (tuple, optional): Mean and standard deviation for normalization.
+
+    Returns:
+    - Tuple[torch.utils.data.Dataset]: Training and validation datasets.
+    """
     transform = [
         transforms.RandomHorizontalFlip(),
         transforms.ToTensor(),
         transforms.Lambda(scale_imgs),
     ]
+
     if name == Case.cifar10_grayscale:
         transform.append(transforms.Grayscale())
     if mean_std is not None:
@@ -125,67 +167,40 @@ def prepare_dataset(name, mean_std=None):
 
     transform = Compose(transform)
 
-    if name in (Case.cifar10, Case.cifar10_grayscale):
-        train_data = CIFAR10(
-            "~/datasets",
-            train=True,
-            transform=transform,
-            download=True,
+    dataset_map = {
+        Case.cifar10: CIFAR10,
+        Case.cifar10_grayscale: CIFAR10,
+        Case.fashion_mnist: FashionMNIST,
+        Case.mnist: MNIST,
+    }
+
+    if name in dataset_map:
+        train_data = dataset_map[name](
+            "~/datasets", train=True, transform=transform, download=True
         )
-        val_data = CIFAR10(
-            "~/datasets",
-            train=False,
-            transform=transform,
-            download=True,
-        )
-    elif name == Case.fashion_mnist:
-        train_data = FashionMNIST(
-            "~/datasets",
-            train=True,
-            transform=transform,
-            download=True,
-        )
-        val_data = FashionMNIST(
-            "~/datasets",
-            train=False,
-            transform=transform,
-            download=True,
-        )
-    elif name == Case.mnist:
-        train_data = MNIST(
-            "~/datasets",
-            train=True,
-            transform=transform,
-            download=True,
-        )
-        val_data = MNIST(
-            "~/datasets",
-            train=False,
-            transform=transform,
-            download=True,
+        val_data = dataset_map[name](
+            "~/datasets", train=False, transform=transform, download=True
         )
     else:
-        raise RuntimeError("Unkown dataset.")
-    # train_data = CelebA(
-    #     "/home/morel/datasets",
-    #     split="train",
-    #     transform=transform,
-    #     download=True,
-    # )
-    # val_data = CelebA(
-    #     "/home/morel/datasets",
-    #     split="val",
-    #     transform=transform,
-    #     download=True,
-    # )
-    # train_data = datasets.MNIST(
-    #     "datasets", train=True, download=True, transform=transform
-    # )
-    # val_data = datasets.MNIST("datasets", train=False, transform=transform)
+        raise RuntimeError("Unknown dataset.")
+
     return train_data, val_data
 
 
-def prepare_toy_data(data_type, n_samples, path="outputs"):
+def prepare_toy_data(
+    data_type: str, n_samples: int, path: str = "outputs"
+) -> list:
+    """
+    Prepares toy data based on the given type and number of samples.
+
+    Parameters:
+    - data_type (str): Type of toy data.
+    - n_samples (int): Number of samples to be generated.
+    - path (str, optional): Directory path for toy data outputs. Defaults to "outputs".
+
+    Returns:
+    - list: Training and validation data.
+    """
     X = inf_train_gen(data_type, batch_size=n_samples, path=path)
     X = torch.tensor(X, dtype=torch.float32)
     x_y = list(train_test_split(X, test_size=0.20, random_state=42))
@@ -193,41 +208,25 @@ def prepare_toy_data(data_type, n_samples, path="outputs"):
     return x_y
 
 
-def prepare_toy_dataset(data_type, n_samples, log_dir):
-    """Load / construct the dataset.
+def prepare_toy_dataset(
+    data_type: str, n_samples: int, log_dir: str
+) -> (torch.utils.data.Dataset, torch.utils.data.Dataset):
+    """
+    Load/construct the dataset.
 
-    Args:
-        data: A Data object.
-        logger: Logger of the simulation.
+    Parameters:
+    - data_type (str): Type of toy data to be prepared.
+    - n_samples (int): Number of samples to be generated.
+    - log_dir (str): Directory path for toy data outputs.
 
     Raises:
-        RuntimeError: Unknown data.data_type.
+    - RuntimeError: If an unknown data_type is provided.
 
     Returns:
-        A tuple made of a training and a validation dataset.
+    - Tuple[torch.utils.data.Dataset]: A tuple made of a training and a validation dataset.
     """
 
     x_train, x_val = prepare_toy_data(data_type, n_samples, path=log_dir)
     x_train, x_val = Dataset(x_train), Dataset(x_val)
 
-    return (x_train, x_val)
-
-
-def get_fashion_mnist_dataloader():
-    batch_size = 128
-
-    train_data, _ = prepare_dataset("fashion_mnist")
-
-    # create dataloader
-    dataloader = DataLoader(train_data, batch_size=batch_size, shuffle=True)
-    return dataloader
-
-
-def get_celeba_dataloader():
-    batch_size = 128
-
-    train_data, _ = prepare_dataset("fashion_mnist")
-
-    # create dataloader
-    dataloader = DataLoader(train_data, batch_size=batch_size, shuffle=True)
-    return dataloader
+    return x_train, x_val
