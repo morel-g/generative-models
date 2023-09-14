98d18e8ea1c2f0346919bd9762bd98faf7b05547
gm <you@example.com>
2023-09-14 06:55:08 +0200

    Update cifar config files

--- config_files/img/cld_cifar_config.py
+++ config_files/img/cld_cifar_config.py
@@ -3,8 +3,7 @@ from config_files.img.default_img_config import get_default_config
 
 CONFIG = get_default_config()
 CONFIG["data_type"] = Case.cifar10
-CONFIG["model_type"] = Case.score_model
-CONFIG["scheme_params"]["beta_case"] = Case.vanilla
+CONFIG["model_type"] = Case.score_model_critical_damped
 CONFIG["scheme_params"]["decay_case"] = Case.vanilla_sigma
 CONFIG["training_params"].update(
     {
